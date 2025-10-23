517559113af105ddbba7facb8e93e6c3c52b888d
Decatalyst247 <seidoavoseh24@gmail.com>
2025-10-23 10:34:18 +0100

    configure Allowed HOSTS now

--- api_project/settings.py
+++ api_project/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='127.0.0.1,localhost',
+).split(',')
 # Application definition
 
 INSTALLED_APPS = [
